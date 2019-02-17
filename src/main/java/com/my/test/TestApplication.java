f0816b27ade3f14eb7acb610d0239425ff953c7e
wangyuqiang <wangyuqiang@carcredit.com.cn>
2019-02-17 12:04:13 +0800

    first commit

---
+++ src/main/java/com/my/test/TestApplication.java
@@ -0,0 +1,16 @@
+package com.my.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+        System.out.println("Spring boot run success");
+    }
+
+}
